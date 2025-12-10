51e31a82a9789430273b8bee96c2963e9cfc760f
Luffy00110 <mustafaege166@gmail.com>
2025-12-10 23:01:02 +0300

    Sistemimizin kulüp kısmının ilk kodları gönderildi.

---
+++ main.py
@@ -0,0 +1,9 @@
+# Akıllı Kampüs Yönetim Sistemi
+# Grup Üyeleri: [İsimlerinizi Buraya Yazabilirsin]
+
+def main():
+    print("--- Akıllı Kampüs Sistemi Başlatılıyor ---")
+    # Modüller buraya eklenecek
+
+if __name__ == "__main__":
+    main()  
