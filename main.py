88381a5dc6f7fac0b186fadd9040290f32005508
Luffy00110 <mustafaege166@gmail.com>
2025-12-10 23:18:15 +0300

    Kulup modülünün ilk kodlarını yazdım.

--- main.py
+++
@@ -1,9 +0,0 @@
-# Akıllı Kampüs Yönetim Sistemi
-# Grup Üyeleri: [İsimlerinizi Buraya Yazabilirsin]
-
-def main():
-    print("--- Akıllı Kampüs Sistemi Başlatılıyor ---")
-    # Modüller buraya eklenecek
-
-if __name__ == "__main__":
-    main()  
